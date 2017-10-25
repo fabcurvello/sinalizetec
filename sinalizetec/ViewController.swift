6f639ea9fef76511bf5c20211359ab910db87c14
Fabrício Curvello <fabriciocurvello@imac-de-fabricio.local>
2017-10-25 10:04:31 -0200

    Initial Commit

---
+++ sinalizetec/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sinalizetec
+//
+//  Created by Fabrício Curvello on 25/10/2017.
+//  Copyright © 2017 Fabrício Curvello. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
